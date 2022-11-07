24c35aeefe824a2ce49f003883bb7d27402cf579
Alexander Popov <popov.a.connect@gmail.com>
2022-11-07 10:57:10 +0000

    Firebase init

---
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,15 @@
+import { signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+  };
+  return (
+    <div>
+      <h1> SIGN IN PAGE </h1>
+      <button onClick={logGoogleUser}> Sign In with Google Popup </button>
+    </div>
+  );
+};
+
+export default SignIn;
