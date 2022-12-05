2835dd35b5a30c4f1816a2d3427a367f0981ff27
PAS240886 <popov.a.connect@gmail.com>
2022-12-05 14:00:54 +0300

    Database categories added

--- src/components/contexts/products.context.jsx
+++ src/components/contexts/products.context.jsx
@@ -1,5 +1,5 @@
-import {createContext, useState} from 'react';
-import PRODUCTS from '../../shop-data.json';
+import {createContext, useState, useEffect} from 'react';
+import {getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.jsx';
 
 export const ProductsContext = createContext({
     products: [],
@@ -7,7 +7,13 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+    useEffect (()=> {
+        const getCategoryMap = async () =>{
+            const categoryMap = await getCategoriesAndDocuments();
+        };
+    
+    }, []);
     const value = {products};
     return (
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
