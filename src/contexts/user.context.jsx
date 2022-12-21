686dc98ad20367c6f3da395fae4a5b1d214e7267
PAS240886 <popov.a.connect@gmail.com>
2022-12-21 13:42:37 +0300

    Redux set-up

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -13,26 +13,28 @@ export const UserContext = createContext({
 
 export const USER_ACTION_TYPES = {
   SET_CURRENT_USER: 'SET_CURRENT_USER'
-}
+};
+
+const INITIAL_STATE = {
+  currentUser: null
+};
 
 const userReducer = (state, action) => {
  const {type, payload} = action;
 
  switch (type){
   case USER.USER_ACTION_TYPES.SET_CURRENT_USER:
+
     return{
       ...state,
       currentUser:payload,
     }
-  
   default:
     throw new Error(`Unhandled type ${type} in userReducer`);
  }
 
-}
-const INITIAL_STATE = {
-  currentUser: null
-}
+};
+
 
 export const UserProvider = ({ children }) => {
   // const [currentUser, setCurrentUser] = useState(null);
@@ -41,7 +43,7 @@ export const UserProvider = ({ children }) => {
   const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE );
   const setCurrentUser = (user) => {
     dispatch ({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
-  }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
